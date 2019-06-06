f64492fb3e1eef98ed1cca3221b081d362095da1
chidha2005 <chidha2005@gmail.com>
2019-06-06 08:29:41 +0530

    Delete MovieRepository.java

--- src/main/java/com/reactive/webflux/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.reactive.webflux.repository;
-
-import com.reactive.webflux.domain.Movie;
-
-import reactor.core.publisher.Flux;
-
-public interface MovieRepository {
-	Flux<Movie> findAll();
-}
